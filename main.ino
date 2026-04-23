46b5c326b0496ac68a7240ea865b956c435c7838
Haley <hlind.engineering@gmail.com>
2026-04-23 10:54:56 -0400

    pwm function began

--- main.ino
+++ main.ino
@@ -12,14 +12,36 @@ using namespace std;
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define LEDC_TIMER0_CONF_REG 0x600190A0 // set reference clock, divider, and duty cycle width register address purpose
+#define LEDC_CONF_REG 0x600190D0 //Enables and selects LEDC clock
+#define LEDC_CH0_CONF0_REG 0x60019000 //idle value, enable, select timer
+#define LEDC_CH0_CONF1_REG 0x6001900C //commits configuration changes
+#define LEDC_CH0_DUTY_REG 0x60019008 // duty cycle
+#define LEDC_INT_CLR_REG 0x600190CC //clear interrupt
+
+#define AUDIO_PIN 33 // CORRECT! 
+#define LED_PIN 14 // CORRECT!
+#define PWM_CHANNEL 0 // Channel 0 select
+#define PWM_RES_BITS 8 // channel 8 select
+
+#define LEDC_CLK_EN_BIT (1u << 31)
+#define LEDC_APB_CLK_SEL_BIT (1u << 0)
+#define LEDC_TIMER0_PARA_UP (1u << 25)
+#define LEDC_SIG_OUT_EN_CH0 (1u << 2)
+#define LEDC_PARA_UP_CH0 (1u << 4)
+#define LEDC_DUTY_START_CH0 (1u << 31)
+#define LEDC_TIMER0_OVF_INT_CLR (1u << 0)
+
+#define LED_UPDATE_HZ 100U
+
 // initialize PCNT periphereal (unit & channel) 
-pcnt_unit_handle_t mypcnt = 0; 
-pcnt_channel_handle_t mychannel = 0;
+pcnt_unit_handle_t mypcnt = 0;  // each unit on the chip can contain multiple channels
+pcnt_channel_handle_t mychannel = 0; // a channel can track pulses on one signal at a time 
 
 // configuration struct 
 pcnt_unit_config_t myunitconfig = {
   .low_limit = -1,
-  .high_limit  = (1 << 14), 
+  .high_limit  = (1 << 14), // high limit for pulse counter - don't change
   .intr_priority = 0, 
   .flags = {
     .accum_count = 0
@@ -27,7 +49,7 @@ pcnt_unit_config_t myunitconfig = {
 };
 
 pcnt_chan_config_t mychannelconfig = {
-  .edge_gpio_num = <pin>, 
+  .edge_gpio_num = <pin>, // single pin to be monitored for pulses is defined here
   .level_gpio_num = -1,
   .flags = {
     .invert_edge_input = 0,
@@ -42,15 +64,18 @@ pcnt_new_channel(mypcnt, &mychannelconfig, &mychannel);
 
 // defines what happens on positive and negtive signal
 // this can be modified by passing PCTN_CHAN...INCREASE as the latter parameter to get more fine tuned reading
-pcnt_channel_set_edge_action(mychannel, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_HOLD);
+// increment count on rise, hold on fall. 
+pcnt_channel_set_edge_action(mychannel, 
+  PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_HOLD);
 
 pcnt_unit_clear(mypcnt); // clear
 pcnt_unit_enable(mypcnt); // ensure its on
 pcnt_unit_start(mypcnt);  // start it up 
 
-// i suppose there will have to be a loop to read, this is how we would do that 
+// i suppose there will have to be a loop to read and then clear, this is how we would do that 
 pcnt_unit_get_count(mypcnt, &val); 
 pcnt_unit_clear_count(mypcnt);
+// must read the count before it reaches 2^14-1 = 16383 count
 
 // docs say we will need 2 tasks, 1 to monitor motor speed, and one to control PWM signal. (loop task to monitor cpu)
 // refer to doc for my structure here
@@ -61,7 +86,14 @@ pcnt_unit_clear_count(mypcnt);
 // initialize obj motor speed handle of taskhandle type 
 TaskHandle_t motor_speed_handle; 
 // create motor_speed task with 64kb mem on stack, highest priority, and address of obj of handle. 
-xTaskCreate(motor_speed,"monitoring the motor speed", 1<<16, 0, configMAX_PRIORITIES -1, &motor_speed_handle);
+xTaskCreate(motor_speed,"monitoring-motor-speed", 1<<16, 0, configMAX_PRIORITIES -1, &motor_speed_handle);
+xTaskCreate(compute_pwm, "compute-pwm", 1<<16, 0, configMAX_PRIORITIES -1, &compute_pwm_handle);
+
+void compute_pwm (void *args) {
+
+
+
+}
 
 // task functons can be setup like so
 void motor_speed ( void *args ) {
@@ -82,22 +114,26 @@ void motor_speed ( void *args ) {
 
 void setup() {
 
-bool system_on = true; 
-uint64_t start_time = esp_timer_get_time(); // I think this should be a float no?
-uint64_t cpu_utilization; 
-int rpm; 
-cout << "Welcome to hell" << "We hope you enjoy your stay" << endl; 
-while(system_on) {
-  // track cpu utilization 
-  float cpu_utilization = taskATotal / (esp_timer_get_time-start_time); 
-  int rpm = (60 * 100) / f; // we need to set/get f here somehow
-  if (button_pressed) {
+  bool system_on = true; 
+  uint64_t start_time = esp_timer_get_time(); // I think this should be a float no?
+  uint64_t cpu_utilization; 
+  int rpm; 
+
+  while(system_on) {
+    // track cpu utilization 
+    float cpu_utilization = taskATotal / (esp_timer_get_time-start_time); 
+    int rpm = (60 * 100) / f; // we need to set/get f here somehow
+    if (button_pressed) {
+
+  }
+
+  rpm += rpm / f;
 
 }
 
-rpm += rpm / f;
-
+void loop() {
+  // runs in FreeRTOS task at lowest priority 
+  // monitor utilization of CPU here
 }
-
 
 }
